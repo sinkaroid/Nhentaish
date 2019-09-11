cdf46d10114199e7721777ac696d16c35d8771f9
sinkaroid <bibsindra@gmail.com>
2019-09-11 08:08:42 +0700

    implemented new grepregex algorithm

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ![](https://img.shields.io/badge/codename-nhentai-hotpink)  
 bash nhentai stealer  
 
-![](https://1.bp.blogspot.com/-_nc5w76xI_Q/XUjMpitFwgI/AAAAAAAAJm0/IDyeJIRT4yc7L7qQJPL94zCAOH1AgnfygCLcBGAs/s1600/Screenshot_61.png)  
+![](https://1.bp.blogspot.com/-KPkuxhX4r90/XXhGBXGUfHI/AAAAAAAAJsA/zPxiDqddgm4vQKEmvS6BXoOrf1Zvcga_QCLcBGAsYHQ/s1600/Screenshot_131.png)  
 
 ```sh
 $ ./nhen.sh #chmod +x if necessary
@@ -10,7 +10,7 @@ $ ./nhen.sh #chmod +x if necessary
 
 ### result
 ```sh
-# directly mkdir as your put code like /177013
+# now your resultname same as title like nhentai site.
 ```  
 
 ## License
--- nhen.sh
+++ nhen.sh
@@ -6,7 +6,7 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2) 
 CYAN=$(tput setaf 6) 
 WHITE=$(tput setaf 7) 
-MAGENTA=$(tput setaf 5) 
+
 
 begin=$(date +"%s")
 
@@ -23,20 +23,29 @@ echo "![Nhentai stealer]!"
 #get
 read -p "${WHITE}code: " kode
 echo -e "\n"
-read -p "${MAGENTA}${kode} Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+read -p "${WHITE}${kode} Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 echo -e "reading source for /${GREEN}$kode ..
 ${WHITE}"
 
-wget -O ${kode}.html https://nhentai.net/g/${kode};
+wget -q -nv -O ${kode}.html https://nhentai.net/g/${kode};
 
+
+var="$(cat ${kode}.html | grep -oP '(?<=<h1>)[^<]*')"
+
+echo ${GREEN}$var
+echo ${WHITE}
+mkdir -pv "${var}" 
 #do
-mkdir $kode
-cat ${kode}.html | grep img | grep -Po '<img src="\K.*?(?=")' | sed 's/\?.*//' | sed 's/t.nhentai/i.nhentai/' | sed 's/t.jpg/.jpg/' | sed 's/t.png/.png/' > ${kode}/links.txt; 
-cd $kode;
-wget -i links.txt
+
+cat ${kode}.html | grep img | grep -Po '<img src="\K.*?(?=")' | sed 's/\?.*//' | sed 's/t.nhentai/i.nhentai/' | sed 's/t.jpg/.jpg/' | sed 's/t.png/.png/' > "${var}"/links.txt | rm ${kode}.html; 
+cd "${var}";
+wget -q -nv -i links.txt | rm links.txt # option -i $argv if necessary show dat process
+
+
 
 #length
 termin=$(date +"%s")
 difftimelps=$(($termin-$begin))
 echo "${WHITE}$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) sec. elapsed ${RED}nhentai stealer"
 
+#283616
